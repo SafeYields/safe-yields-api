7f31ec0a46ad8340bb031b70bd2ac64dd08e409b
crypt0grapher <7blockchains@gmail.com>
2023-02-12 00:52:13 +0800

    feat:  network connection config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,23 @@ import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EthersModule } from 'nestjs-ethers';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     CacheModule.register(),
-    EthersModule.forRoot(),
+    EthersModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => {
+        return {
+          token: config.get('CHAIN_NAME'),
+          custom: config.get('CHAIN_URL'),
+          useDefaultProvider: false,
+        };
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
